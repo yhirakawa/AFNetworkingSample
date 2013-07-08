e13c77dc60f140097a5a02e57f5f0ddff79d7847
Yuta Hirakawa <yhirakawa@sonix.asia>
2013-07-08 23:27:34 +0900

    Initial Commit

---
+++ AFNetworkingSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AFNetworkingSample
+//
+//  Created by yuta hirakawa on 2013/07/08.
+//  Copyright (c) 2013年 yhirakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
