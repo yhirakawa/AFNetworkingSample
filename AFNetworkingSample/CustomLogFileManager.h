2385ab3b8c8c82941710f82b57c9842ee778da06
Yuta Hirakawa <yhirakawa@sonix.asia>
2013-07-18 21:58:10 +0900

    first commit

---
+++ AFNetworkingSample/CustomLogFileManager.h
@@ -0,0 +1,13 @@
+//
+//  CustomLogFileManager.h
+//  AFNetworkingSample
+//
+//  Created by yuta hirakawa on 2013/07/18.
+//  Copyright (c) 2013年 yhirakawa. All rights reserved.
+//
+
+#import "DDFileLogger.h"
+
+@interface CustomLogFileManager : DDLogFileManagerDefault
+
+@end
---
+++ AFNetworkingSample/CustomLogFileManager.m
@@ -0,0 +1,13 @@
+//
+//  CustomLogFileManager.m
+//  AFNetworkingSample
+//
+//  Created by yuta hirakawa on 2013/07/18.
+//  Copyright (c) 2013年 yhirakawa. All rights reserved.
+//
+
+#import "CustomLogFileManager.h"
+
+@implementation CustomLogFileManager
+
+@end
