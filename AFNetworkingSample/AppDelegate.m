b07a7de0825ff23cca7784f9060f943b4ff453cc
Yuta Hirakawa <yhirakawa@sonix.asia>
2013-11-07 21:04:28 +0900

    Add Lumberjack

--- AFNetworkingSample/AppDelegate.m
+++ AFNetworkingSample/AppDelegate.m
@@ -7,16 +7,97 @@
 //
 
 #import "AppDelegate.h"
+#import "AFNetworking.h"
+#import "DDTTYLogger.h"
+#import "CustomLogFileManager.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // DDLog初期設定
+    CustomLogFileManager *logFileManager = [[CustomLogFileManager alloc] init];
+	// ファイルにログを出力
+	DDFileLogger *fileLogger = [[DDFileLogger alloc] initWithLogFileManager:logFileManager];
+    // 一日に一回ログファイルを更新
+	// fileLogger.rollingFrequency =   60 * 60 * 24;
+    // 10秒に一回ログファイルを更新
+    fileLogger.rollingFrequency =   10;
+    // ログファイルを残す数
+	fileLogger.logFileManager.maximumNumberOfLogFiles = 7;
+    // Xcodeのコンソールにログを出力
+	[DDLog addLogger:[DDTTYLogger sharedInstance]];
+	[DDLog addLogger:fileLogger];
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    // ios標準ボタンを作成
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    // 配置を決定 (x位置, y位置, 横幅, 高さ)
+    [btn setFrame :CGRectMake(50, 100, 80, 40)];
+    // ボタンの文字列を指定
+    [btn setTitle :@"Get json!" forState:UIControlStateNormal];
+    // ボタンを押下した際のイベントを指定
+    [btn addTarget:self
+            action:@selector(getJson:) forControlEvents:UIControlEventTouchUpInside];
+    // ボタンをwindowに追加
+    [self.window addSubview:btn];
+    
+    // ボタンと同じように結果表示のためにUILabel追加
+    self.label = [[UILabel alloc] init];
+    [self.label setFrame:CGRectMake(50, 200, 200, 30)];
+    [self.label setText:@"Please push button"];
+    [self.window addSubview:self.label];
     [self.window makeKeyAndVisible];
+    
     return YES;
+}
+
+
+-(void)getJson:(UIButton*)button{
+    // ネットワーク状態を確認する
+    AFHTTPClient *httpClient = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"http://google.com"]];
+    NSLog(@"Network reach %d",[httpClient networkReachabilityStatus]);
+    // networkReachabilityStatusには下記状態がある
+    // AFNetworkReachabilityStatusReachableViaWiFi wifi
+    // AFNetworkReachabilityStatusReachableViaWWAN 3G
+    // AFNetworkReachabilityStatusNotReachable     繋がってない
+    // ネットワークに繋がっていない場合はアラートを表示する
+    if ( [httpClient networkReachabilityStatus] == AFNetworkReachabilityStatusNotReachable ) {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@""
+                              message:@"ネットワークに繋がっていません\r\n設定をお確かめの上、もう一度お試し下さい。"
+                              delegate:nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
+    // open apiを使ってjsonを取得
+    NSURL *url = [NSURL URLWithString:@"http://clip.eventcast.jp/api/v1/Search?Keyword=Google&Format=json"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setTimeoutInterval:1]; // ios6からタイムアウト設定が行える
+    // content typeを指定
+    [AFJSONRequestOperation addAcceptableContentTypes:[NSSet setWithObject:@"text/html"]];
+    AFJSONRequestOperation *operation =
+    [AFJSONRequestOperation JSONRequestOperationWithRequest:request
+                                                    success:^(NSURLRequest *request, NSHTTPURLResponse *response, id json) {
+                                                        // 成功した場合
+                                                        // UILabelに表示 AFNetworkingはjsonを使いやすい形に渡してくれる
+                                                        [self.label setText:
+                                                         [NSString stringWithFormat:
+                                                          @"TotalResult : %@", [json valueForKeyPath:@"TotalResult"]
+                                                         ]
+                                                        ];
+                                                        DDLogInfo(@"TotalResult : %@", [json valueForKeyPath:@"TotalResult"]);
+                                                    } failure:^(NSURLRequest *req, NSHTTPURLResponse *res, NSError *e, id json) {
+                                                        // 失敗した場合、この時点でネットワークに繋がっていない、タイムアウト、urlが間違っている等でここに来る
+                                                        // NSLog(@"%@", e.localizedDescription);
+                                                        DDLogError(@"%@", e.localizedDescription);
+                                                    }];
+    [operation start];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
--- AFNetworkingSample/CustomLogFileManager.m
+++ AFNetworkingSample/CustomLogFileManager.m
@@ -7,7 +7,41 @@
 //
 
 #import "CustomLogFileManager.h"
+#import "AFNetworking.h"
 
 @implementation CustomLogFileManager
+
+// ログファイルが更新される際に呼ばれる
+-(void)didRollAndArchiveLogFile:(NSString *)logFilePath
+{
+    // ログファイルのパスからログ内容を取得
+    DDLogFileInfo *info = [DDLogFileInfo logFileWithPath:logFilePath];
+    NSString* content = [NSString stringWithContentsOfFile:info.filePath
+                                                  encoding:NSUTF8StringEncoding
+                                                     error:NULL];
+    // ログを送信
+    [self postLogData:content];
+}
+
+-(void)postLogData:(NSString *) data
+{
+    // ログデータを表示するのでここはNSLogを使用する
+    NSLog(@"post data : %@", data);
+    // ※https://sample.urlは存在しないURLですので、失敗になりますが
+    // このようにエラーログ等をサーバに送る事でログ収集が可能です
+    NSURL *url = [NSURL URLWithString:@"https://sample.url"];
+    AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:url];
+    NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
+                            data, @"log",
+                            nil];
+    [httpClient postPath:@"/log/save" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        // 成功時
+        NSString *responseStr = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        DDLogVerbose(@"Request Successful, response '%@'", responseStr);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        // 失敗時
+        DDLogError(@"[HTTPClient Error]: %@", error.localizedDescription);
+    }];
+}
 
 @end
